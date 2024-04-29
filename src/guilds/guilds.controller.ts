2942a13f06e681fb5d4fc3076e4ade4b7f7cc1c9
mohand-b <mohandbdj@gmail.com>
2024-04-29 23:59:27 +0200

    feat: Add guild member

--- src/guilds/guilds.controller.ts
+++ src/guilds/guilds.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { GuildsService } from './guilds.service';
 import { Guild } from './entities/guild.entity';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/entities/user.entity';
 
 @Controller('guilds')
 export class GuildsController {
-  constructor(private readonly guildsService: GuildsService) {}
+  constructor(
+    private readonly guildsService: GuildsService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Get()
   findAll(): Promise<Guild[]> {
     return this.guildsService.findAll();
+  }
+
+  @Get(':guildId/members')
+  getGuildMembers(
+    @Param('guildId', ParseIntPipe) guildId: number,
+  ): Promise<User[]> {
+    return this.usersService.findMembersByGuild(guildId);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,20 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Repository } from 'typeorm';
+import { Not, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserRole } from './enum/user-role.enum';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>,
+    private userRepository: Repository<User>,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const { username } = createUserDto;
-    const existingUser = await this.usersRepository.findOne({
+    const existingUser = await this.userRepository.findOne({
       where: { username },
     });
 
@@ -21,16 +22,16 @@ export class UsersService {
       throw new ConflictException('Username is already taken.');
     }
 
-    const user = this.usersRepository.create(createUserDto);
-    return await this.usersRepository.save(user);
+    const user = this.userRepository.create(createUserDto);
+    return await this.userRepository.save(user);
   }
 
   async save(user: User): Promise<User> {
-    return await this.usersRepository.save(user);
+    return await this.userRepository.save(user);
   }
 
   async findAll(): Promise<any[]> {
-    const users = await this.usersRepository.find({ relations: ['guild'] });
+    const users = await this.userRepository.find({ relations: ['guild'] });
     return users.map((user) => ({
       id: user.id,
       username: user.username,
@@ -41,10 +42,19 @@ export class UsersService {
   }
 
   async findOneById(id: number): Promise<User> {
-    return this.usersRepository.findOneBy({ id });
+    return this.userRepository.findOneBy({ id });
   }
 
   async findOneByUsername(username: string): Promise<User> {
-    return this.usersRepository.findOneBy({ username });
+    return this.userRepository.findOneBy({ username });
+  }
+
+  async findMembersByGuild(guildId: number): Promise<User[]> {
+    return this.userRepository.find({
+      where: {
+        guild: { id: guildId },
+        role: Not(UserRole.CANDIDATE),
+      },
+    });
   }
 }
